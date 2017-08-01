a40eca4ac0ddc86d3e9dbefec5a96d08b7b4fa23
subbat9710 <teela.subba1991@gmail.com>
2017-08-01 13:03:17 -0400

    My first personal website.

---
+++ local_env.rb
@@ -0,0 +1,8 @@
+ENV['host'] = 'myblogdb.czlwnzedcojz.us-west-2.rds.amazonaws.com'
+ENV['port'] = '5432'
+ENV['db_name'] = 'myblogdb'
+ENV['user'] = 'subbat9710'
+ENV['password'] = 'master12'
+
+
+#this should be secure to protect the informations. 
