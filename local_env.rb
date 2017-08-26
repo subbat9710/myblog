ec3c4f21e2efa6567ef982d5bda8b02a9c7c2c5b
subbat9710 <teela.subba1991@gmail.com>
2017-08-25 22:30:48 -0400

    making some changes

--- local_env.rb
+++ local_env.rb
@@ -1,6 +1,6 @@
-ENV['host'] = 'blogdb.czlwnzedcojz.us-west-2.rds.amazonaws.com'
+ENV['host'] = 'myblogdb.czlwnzedcojz.us-west-2.rds.amazonaws.com'
 ENV['port'] = '5432'
-ENV['db_name'] = 'blogdb'
+ENV['db_name'] = 'myblogdb'
 ENV['user'] = 'subbat9710'
 ENV['password'] = 'master12'
 
